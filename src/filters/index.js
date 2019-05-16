c229374a53a022f1f5e585039a58a86288655293
shengchuang <sheng.chuang@vpclub.cn>
2019-05-16 17:19:56 +0800

    rebuild: 再来一次

--- src/filters/index.js
+++
@@ -1,7 +0,0 @@
-const files = require.context('./', true, /\.js$/)
-let filters = {};
-files.keys().map(item => {
-  let name = item.replace(/\.\/(\w+)\.js/g, '$1')
-  filters[name] = require(`${item}`).default;
-})
-export default filters
